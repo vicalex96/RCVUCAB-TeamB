531553cf65896b7c27c1262bae554523ce34d5d3
Andrea Vasquez <andreavasquezamancha@gmail.com>
2022-06-24 21:12:39 -0400

    modificacion de DAO y creacion de controllers

--- app-proveedor/proveedor/Persistence/DAOs/Interfaces/ICotizacionParteDAO.cs
+++ app-proveedor/proveedor/Persistence/DAOs/Interfaces/ICotizacionParteDAO.cs
@@ -6,7 +6,7 @@ namespace administracion.Persistence.DAOs
 {
     public interface ICotizacionParteDAO
     {
-        public string actualizarCotizacionParte(Guid CotizacionParteID, EstadoCotPt estado);
+    
         public string createCotizacionParte(CotizacionParteDTO cotPt);
         public List<CotizacionParteDAO> GetCotizacionPartes();
         public CotizacionParteDTO GetCotizacionPartesByestado(EstadoCotPt estado);
