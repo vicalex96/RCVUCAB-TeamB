211f5b48455b7017b8c57cde2830792affffe392
vicalex96 <vadjdn@gmail.com>
2022-07-07 10:59:44 -0400

    Agregado el codigo incial del levantamiento
    El levantamiento ya implementa los metodos necesario para ejecutar la mayor parte de su funcionalidad
    Aun falta reacomodarlo y agregle sus test

---
+++ app-levantamiento/levantamiento/BussinessLogic/DTOs/Solicitudes/solicitudesRespacionSimpleDTO.cs
@@ -0,0 +1,9 @@
+namespace levantamiento.BussinesLogic.DTOs
+{
+    public class SolicitudesRespacionRegisterDTO
+    {
+        public Guid Id {get; set;}
+        public Guid incidenteId {get; set;}
+        public Guid vehiculoId {get; set;}
+    }
+}
